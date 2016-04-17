ddad1a472f437f20e38da35fe1ddf8cf6426c592
GuruMeditation02 <dunor449@virginmedia.com>
2016-04-17 18:43:56 +0100

    Version number update

--- Lumberjack.info.php
+++ Lumberjack.info.php
@@ -4,7 +4,7 @@ $info = array(
 	// The module title, typically a little more descriptive than the class name
 	'title' => 'Lumberjack',
 	// version number (integer)
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	// author
 	'author' => 'GuruMeditation',
 	// summary is brief description of what this module is
